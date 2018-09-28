908cafd78422fbefa8bfa58a61b652a463873204
Khakov Rustam <khakov.rustam@gmail.com>
2018-09-28 23:38:13 +0300

    Spark: first lesson

---
+++ src/main/scala/TestScala.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * @author Rustam Khakov
+  */
+object TestScala {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    conf.setAppName("Spark Hello World")
+    conf.setMaster("local[2]")
+
+    val sc = new SparkContext(conf)
+    println(sc) }
+}
